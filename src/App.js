8b6908f81d6f0518850dd86ce214ed595cc955dc
Fuller1212 <fullermorrison@gmail.com>
2022-08-01 14:52:28 -0500

    Created DisplayEntries Component and Instantiated Component on App Component

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+
+function App() {
+
+const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
+
+  return (
+    <div>
+      <table>
+        <thead> 
+          <tr>
+            <th> Entry Number</th>
+            <th> Weight</th>
+            <th> date</th>
+          </tr>
+        </thead>
+          <tbody> 
+            {entries.map((entry, index) => {
+              return (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{entry.weight}</td>
+                  <td>{entry.date}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
